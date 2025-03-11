873a3f5e038efc56fa12501b96d3de98b6ec5284
Curtis <curtisjohn718@aol.com>
2025-03-11 16:53:11 -0400

    Added: final working version

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && <p>{bio}</p>}
+      {/* {bio && <p>{bio}</p>} */}
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* /* add your <Links /> component here */}
       <Links 
